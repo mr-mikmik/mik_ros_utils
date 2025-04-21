92836c670842885bdee4a24a087438d03a79b8a9
mr-mikmik <aglaner@hotmail.com>
2025-04-21 16:11:49 -0400

    Refractoring GelslimCameraParser from mik_ros_utils to gelslim_ros.

--- src/mik_ros_utils/camera_utils/camera_parsers/__init__.py
+++ src/mik_ros_utils/camera_utils/camera_parsers/__init__.py
@@ -2,5 +2,4 @@ from .camera_parser_base import CameraParserBase
 from .depth_camera_parser_base import DepthCameraParserBase
 from .realsense_camera_parser import RealSenseCameraParser
 from .picoflexx_camera_parser import PicoFlexxCameraParser
-from .gelslim_camera_parser import GelslimCameraParser
 from .photoneo_motioncam_parser import PhotoneoMotionCamParser
