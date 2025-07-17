fc766bd64a9be6a98ef87b13b069f601b40d89f1
mr-mikmik <aglaner@hotmail.com>
2025-07-17 10:14:00 -0400

    Update RealSenseCameraParser to record infra camera info and infra frames

--- src/mik_ros_utils/camera_utils/camera_parsers/realsense_camera_parser.py
+++ src/mik_ros_utils/camera_utils/camera_parsers/realsense_camera_parser.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
 import os
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 
 from mik_ros_utils.camera_utils.camera_parsers.depth_camera_parser_base import DepthCameraParserBase
-from mik_tools.recording_utils.data_recording_wrappers import ImageSelfSavedWrapper
+from mik_tools.recording_utils.data_recording_wrappers import ImageSelfSavedWrapper, CameraInfoSelfSavedWrapper
 import mik_tools.recording_utils.recording_utils as record_utils
 
 
@@ -23,6 +23,24 @@ class RealSenseCameraParser(DepthCameraParserBase):
         infra2_img = self._get_topic_data('infra2')
         return infra2_img
 
+    def get_camera_info_infra1(self):
+        if self.infra:
+            camera_info_infra1 = self._get_topic_data('infra1_info')
+            if camera_info_infra1 is None:
+                raise ValueError("Infrared camera info not available.")
+            return camera_info_infra1
+        else:
+            return None
+
+    def get_camera_info_infra2(self):
+        if self.infra:
+            camera_info_infra2 = self._get_topic_data('infra2_info')
+            if camera_info_infra2 is None:
+                raise ValueError("Infrared camera info not available.")
+            return camera_info_infra2
+        else:
+            return None
+
     def record(self, fc=None, **kwargs):
         super().record(fc=fc, **kwargs)
         if self.infra:
@@ -37,6 +55,30 @@ class RealSenseCameraParser(DepthCameraParserBase):
             return None
         else:
             return super().record_image_depth(fc=fc)
+
+    def record_camera_info(self, fc=None):
+        if fc is None:
+            fc = self.counter['camera_info']
+        camera_info_color = self.get_camera_info_color()
+        record_utils.record_camera_info_color(camera_info_color, save_path=self.save_path, scene_name=self.scene_name,
+                                              camera_name=self.camera_name, fc=fc)
+        combined_info = {
+            'color': camera_info_color,
+        }
+        if self.infra:
+            camera_info_infra1 = self.get_camera_info_infra1()
+            camera_info_infra2 = self.get_camera_info_infra2()
+            record_utils.record_camera_info(camera_info_infra1, save_path=self.save_path, scene_name=self.scene_name, camera_name=self.camera_name, info_name='camera_info_infra1', fc=fc)
+            record_utils.record_camera_info(camera_info_infra2, save_path=self.save_path, scene_name=self.scene_name, camera_name=self.camera_name, info_name='camera_info_infra2', fc=fc)
+            combined_info['infra1'] = camera_info_infra1
+            combined_info['infra2'] = camera_info_infra2
+        else:
+            camera_info_depth = self.get_camera_info_depth()
+            # record DEPTH camera info
+            record_utils.record_camera_info_depth(camera_info_depth, save_path=self.save_path, scene_name=self.scene_name, camera_name=self.camera_name, fc=fc)
+            combined_info['depth'] = camera_info_depth
+        self.counter['camera_info'] += 1
+        return combined_info
 
     def record_infra(self, fc=None):
         """
@@ -86,6 +128,14 @@ class RealSenseCameraParser(DepthCameraParserBase):
         infra_topic = '/{}/infra1/image_rect_raw'.format(self.camera_name)
         return infra_topic
 
+    def _get_infra1_camera_info_topic(self):
+        infra1_info_topic = '/{}/infra1/camera_info'.format(self.camera_name)
+        return infra1_info_topic
+
+    def _get_infra2_camera_info_topic(self):
+        infra2_info_topic = '/{}/infra2/camera_info'.format(self.camera_name)
+        return infra2_info_topic
+
     def _get_depth_topic(self):
         if self.aligned_depth:
             depth_topic = '/{}/aligned_depth_to_color/image_raw'.format(self.camera_name)
@@ -102,7 +152,9 @@ class RealSenseCameraParser(DepthCameraParserBase):
         return color_info_topic
 
     def _get_depth_info_topic(self):
-        if self.aligned_depth:
+        if self.infra:
+            return None
+        elif self.aligned_depth:
             depth_info_topic = '/{}/aligned_depth_to_color/camera_info'.format(self.camera_name)
         else:
             depth_info_topic = '/{}/depth/camera_info'.format(self.camera_name)
@@ -117,6 +169,8 @@ class RealSenseCameraParser(DepthCameraParserBase):
             added_topics = {
                 'infra1': self._get_infra1_topic(),
                 'infra2': self._get_infra2_topic(),
+                'infra1_info': self._get_infra1_camera_info_topic(),
+                'infra2_info': self._get_infra2_camera_info_topic(),
             }
             topics.update(added_topics)
             # remove the depth topic and depth_infro since both are not published at the same time.
@@ -129,6 +183,8 @@ class RealSenseCameraParser(DepthCameraParserBase):
         added_msgs_types = {
             'infra1': Image,
             'infra2': Image,
+            'infra1_info': CameraInfo,
+            'infra2_info': CameraInfo,
         }
         msgs_types.update(added_msgs_types)
         return msgs_types
@@ -138,6 +194,8 @@ class RealSenseCameraParser(DepthCameraParserBase):
         added_processors = {
             'infra1': self._process_image_msg,
             'infra2': self._process_image_msg,
+            'infra1_info': self._process_camera_info_msg,
+            'infra2_info': self._process_camera_info_msg,
         }
         processors.update(added_processors)
         return processors
@@ -147,6 +205,9 @@ class RealSenseCameraParser(DepthCameraParserBase):
         added_wrappers = {
             'infra1': ImageSelfSavedWrapper,
             'infra2': ImageSelfSavedWrapper,
+            'infra1_info': CameraInfoSelfSavedWrapper,
+            'infra2_info': CameraInfoSelfSavedWrapper,
+
         }
         data_wrappers.update(added_wrappers)
         return data_wrappers
@@ -155,9 +216,22 @@ class RealSenseCameraParser(DepthCameraParserBase):
         additional_data_params = super()._get_additional_data_params()
         additional_data_params['infra1'] = {'image_name': 'infra1'}
         additional_data_params['infra2'] = {'image_name': 'infra2'}
+        additional_data_params['infra1_info'] = {'info_name': 'camera_info_infra1'}
+        additional_data_params['infra2_info'] = {'info_name': 'camera_info_infra2'}
         return additional_data_params
 
     def _process_color_image_data(self, ros_data):
         np_arr = np.fromstring(ros_data.data, np.uint8)
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         return image_np
+
+    def get_camera_frames(self):
+        """
+        Get the camera frames including color, depth, and infrared images.
+        Returns:
+            dict: A dictionary containing the camera frames.
+        """
+        frames = super().get_camera_frames()
+        if self.infra:
+            frames += [f'{self.camera_name}_infra1_frame', f'{self.camera_name}_infra2_frame', f'{self.camera_name}_infra1_optical_frame', f'{self.camera_name}_infra2_optical_frame']
+        return frames
